8ef06df722bb76cb2714fe1c117371aa41742da4
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2024-04-24 14:17:28 -0700

    Organize files and add infinite loop example

--- benchmark/peggy_example/LoopPeeling.java
+++ benchmark/peggy_example/LoopPeeling.java
@@ -1,22 +1,22 @@
 class LoopPeeling {
-    public int original (int N) {
+    public int original (int n) {
         int x = 0;
         int i = 0;
-        while (i < N) {
+        while (i < n) {
             x += 5;
             i++;
         }
         return x;
     }
 
-    public int expected (int N) {
+    public int expected (int n) {
         int x = 0;
-        if (0 >= N) {
+        if (0 >= n) {
             x = 0;
         } else {
             x = 5;
             int i = 1;
-            while (i < N) {
+            while (i < n) {
                 x += 5;
                 i++;
             }
