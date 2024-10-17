ce428b31d5a9b0cbf8a40c80e622577435df3a88
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2024-10-17 15:23:56 -0700

    add analyses to peggy

--- benchmark/passing/FunctionInlining.java
+++ benchmark/passing/FunctionInlining.java
@@ -1,10 +1,10 @@
 public class FunctionInlining {
-    public int foo(int x) {
-        return x + 1;
+    public int foo() {
+        return 1;
     }
 
     public int original(int x) {
-        return foo(x) + 1;
+        return foo() + 1;
     }
 
     public int expected(int x) {
--- run_peggy_optimizer.sh
+++ run_peggy_optimizer.sh
@@ -1,3 +1,3 @@
 #
 # javac $@.java && 
-java -Xmx2000m -cp .:peggy_1.0.jar:polybench:benchmark:benchmark/passing:benchmark/failing peggy.optimize.java.Main -axioms axioms/java_arithmetic_axioms.xml:axioms/java_operator_axioms.xml:axioms/java_operator_costs.xml:axioms/java_util_axioms.xml -O2 $@ -tmpFolder tmp  -pb glpk -activate "inline:inlineall:livsr:binop:constant"
+java -Xmx2000m -cp .:peggy_1.0.jar:polybench:benchmark:benchmark/passing:benchmark/failing peggy.optimize.java.Main -axioms axioms/java_arithmetic_axioms.xml:axioms/java_operator_axioms.xml:axioms/java_operator_costs.xml:axioms/java_util_axioms.xml -O2 $@ -tmpFolder tmp  -pb glpk -activate "inlineall:livsr:binop:constant" -glpkPath "/glpk-5.0/examples/glpsol"
