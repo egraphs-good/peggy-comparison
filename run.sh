72c88d74c495b3a987c20cefd980691ffd8b40eb
oflatt <oflatt@gmail.com>
2025-01-29 09:30:54 -0800

    try test nightly with setup.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 docker start peggy
-conda activate peggy
 python3 scripts/benchmark_peggy.py
--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ image_name=peggy
 # If you use a different name for the container, you must also update
 # config.py.
 container_name=peggy
-conda_env_name=peggy
+
+# remove any docker images named peggy
+docker rm -f $container_name || true
 
 # Make the Docker image and container needed to run peggy (peggy needs
 # Java 6).
@@ -17,8 +19,3 @@ docker build -t $image_name .
 
 # The container will have a volume corresponding to this directory.
 docker run -d -v "$DIR:/peggy-comparison"  --name $container_name -i -t $image_name
-
-# Create the python environment needed to run the benchmark scripts
-conda create -n $conda_env_name python=3.13.0 matplotlib=3.9.2 pandas=2.2.3
-
-conda init
