d498242c3b7d1bcabdc0367a6df8128ca11e0224
Kirsten <32720576+kirstenmg@users.noreply.github.com>
2025-01-31 16:47:35 -0500

    format run script

--- peggy/run_peggy_optimizer.sh
+++ peggy/run_peggy_optimizer.sh
@@ -1,3 +1,9 @@
 #
 # javac $@.java && 
-java -Xmx2000m -cp /peggy-comparison/peggy:/peggy-comparison/peggy/peggy_1.0.jar:/peggy-comparison/java/benchmark peggy.optimize.java.Main -axioms /peggy-comparison/peggy/axioms/java_arithmetic_axioms.xml:/peggy-comparison/peggy/axioms/java_operator_axioms.xml:/peggy-comparison/peggy/axioms/java_operator_costs.xml:/peggy-comparison/peggy/axioms/java_util_axioms.xml -O2 $@ -tmpFolder tmp  -pb glpk -activate "inlineall:livsr:binop:constant"
+java -Xmx2000m -cp /peggy-comparison/peggy:/peggy-comparison/peggy/peggy_1.0.jar:/peggy-comparison/java/benchmark \
+    peggy.optimize.java.Main \
+    -axioms /peggy-comparison/peggy/axioms/java_arithmetic_axioms.xml:/peggy-comparison/peggy/axioms/java_operator_axioms.xml:/peggy-comparison/peggy/axioms/java_operator_costs.xml:/peggy-comparison/peggy/axioms/java_util_axioms.xml \
+    -O2 $@ \
+    -tmpFolder tmp \
+    -pb glpk \
+    -activate "inlineall:livsr:binop:constant"
